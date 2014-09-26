064ca9da69d9ce59ed1cf872d60d29a955e3eef8
Mark Jackson <jacks205@mail.chapman.edu>
2014-09-26 02:42:20 -0700

    Initial commit of all assets

---
+++ Assets/Characters/Girl/Scripts/GunController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour {
+	public Sprite angle45;
+	public Sprite angle90;
+	public Sprite angle135;
+	public Sprite angle180;
+	public Sprite angle225;
+	public Sprite angle270;
+	public Sprite angle315;
+	public Sprite angle360;
+	SpriteRenderer spriteRend;
+	Animator gunAngle;
+
+
+	// Use this for initialization
+	void Start () {
+		spriteRend = GetComponent<SpriteRenderer> ();
+		gunAngle = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 mouse = Camera.main.ScreenToViewportPoint(Input.mousePosition);        //Mouse position
+		Vector3 objpos = Camera.main.WorldToViewportPoint (transform.position);        //Object position on screen
+		Vector2 relobjpos = new Vector2(objpos.x - 0.5f,objpos.y - 0.5f);           //Set coordinates relative to object
+		Vector2 relmousepos = new Vector2 (mouse.x - 0.5f,mouse.y - 0.5f) - relobjpos;
+		float angle = Vector2.Angle (Vector2.up, relmousepos);  //Angle calculation
+		if (relmousepos.x > 0)
+			angle = 360-angle;
+		gunAngle.SetFloat ("MouseAngle", angle);
+		Debug.Log (angle);
+//		if (angle <= 360 && angle > 300) {
+//			spriteRend.sprite = angle45;
+//			Debug.Log (spriteRend.sprite.name);
+//		} else if (angle < 300 && angle >= 240) {
+//			spriteRend.sprite = angle90;
+//			Debug.Log (spriteRend.sprite.name);
+//		}
+	}
+
+}
---
+++ Assets/Characters/Girl/Scripts/HeroController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour {
+
+	public float maxSpeed = 5;
+	public Animator legAnimator = null;
+	public Animator upperBodyAnimator = null;
+	bool facingRight = true;
+
+	Animator anim;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask  whatIsGround;
+	public float jumpForce = 700;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+//		anim.SetBool ("Ground", grounded);
+//		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+		float move = Input.GetAxis ("Horizontal");
+		SetRunningSpeed(Mathf.Abs (move));
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y); 
+		if (move > 0 && !facingRight) {
+			Flip ();
+		} else if (move < 0 && facingRight) {
+			Flip ();
+		}
+	}
+
+	void Update(){
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetBool("Ground", false);
+			rigidbody2D.AddForce(new Vector2(0,jumpForce));
+		}
+	}
+
+	void Flip(){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void SetRunningSpeed(float speed){
+		legAnimator.SetFloat ("Speed", speed);
+		upperBodyAnimator.SetFloat ("Speed", speed);
+	}
+
+}
